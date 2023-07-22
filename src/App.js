b0425288c06b0346d7a4d70311c981f880e538bd
Artem Androsenko <tyoma.and@gmail.com>
2023-07-22 15:03:41 +0300

    Add author and date review for message component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import CreateNewMessage from './components/CreateNewMessage';
 
 function App() {
     const [review, setReview] = useState([
-        { msg: "Hey Eva! You're cool. Nice pic!" },
         {
-            msg: 'Thanks for your services! We really liked the ocean view room. We hope to cooperate in the near future. We are sure you will do everything to make our next holiday fantastic.',
+            name: 'Samuel Jackson',
+            msg: "Hey Eva! You're cool. Nice pic!",
+            date: '13 Apr 2022'
         },
         {
+            name: 'Angela Deimon',
+            msg: 'Thanks for your services! We really liked the ocean view room. We hope to cooperate in the near future. We are sure you will do everything to make our next holiday fantastic.',
+            date: '10 Apr 2022'
+        },
+        {
+            name: 'Roland Harris',
             msg: 'Eva, hello! There is such a question: How can I contact you if I am abroad in roaming?',
+            date: '8 Apr 2022'
         },
     ]);
 
--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -3,9 +3,13 @@ import classes from './Message.module.css';
 const Message = (props) => {
     return (
         <div className={classes.msgContainer}>
+            <p>
+                <strong>{props.review.name}</strong>
+                <span className={classes.msg_date}>{props.review.date}</span>
+            </p>
             <p className={classes.msg}>{props.review.msg}</p>
         </div>
     );
-}
+};
 
 export default Message;
