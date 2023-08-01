b00319a55420d4989f0e552b41689d60cc847e2e
Artem Androsenko <tyoma.and@gmail.com>
2023-08-01 12:55:49 +0300

    Add new logic for CustomerLink component

--- src/components/UI/link/CustomLink.jsx
+++ src/components/UI/link/CustomLink.jsx
@@ -1,12 +1,26 @@
 import classes from './CustomLink.module.css';
 
 const CustomLink = (props) => {
-
     const { link, text } = props;
 
+    const linkHandler = (e) => {
+        e.preventDefault();
+
+        if (props.active) return;
+        props.onClick();
+    }
+
     return (
-        <a href={link} className={classes.a}>{ text }</a>
+        <a
+            href={link}
+            onClick={linkHandler}
+            className={
+                props.active ? `${classes.active} ${classes.a}` : `${classes.a}`
+            }
+        >
+            {text}
+        </a>
     );
-}
+};
 
 export default CustomLink;
